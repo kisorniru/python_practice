b42c42b7e93366a47e651ec5c05fa801750cfced
kisorniru <kisorniru@gmail.com>
2026-07-06 12:31:35 +0600

    class teacher CURD is in progress

--- python-flask-course/flask-103/models/class_teacher.py
+++ python-flask-course/flask-103/models/class_teacher.py
@@ -4,3 +4,10 @@ class ClassTeacher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     class_id = db.Column(db.Integer, db.ForeignKey('school_class.id'), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "class_id": self.class_id,
+            "teacher_id": self.teacher_id
+        }
--- python-flask-course/flask-103/routes/class_teacher.py
+++ python-flask-course/flask-103/routes/class_teacher.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from models import db, ClassTeacher
 
 # Create a Blueprint for the class_teacher routes
 class_teacher_bp = Blueprint("class_teacher", __name__)
 
 @class_teacher_bp.route("/")
-def class_teacher():
-    return "This is the class teacher route."
+def index():
+    # Query all ClassTeacher records from the database
+    class_teachers = ClassTeacher.query.all()
+
+    # Convert the records to a list of dictionaries
+    class_teachers_list = [class_teacher.to_dict() for  class_teacher in class_teachers]
+
+    response_data = {
+        'message': 'List of all class teachers',
+        'class_teachers': class_teachers_list
+    }
+
+    return jsonify(response_data), 200
